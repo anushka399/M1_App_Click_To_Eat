de176a8e603c1072b796ba749596a4de8c1e65bb
anushka399 <94168929+anushka399@users.noreply.github.com>
2021-11-24 13:19:32 +0530

    Update mypro.h

--- 3_Implementation/inc/mypro.h
+++ 3_Implementation/inc/mypro.h
@@ -4,18 +4,14 @@
  * @brief 
  * @version 0.1
  * @date 2021-11-24
- * 
  * @copyright Copyright (c) 2021
- * 
  */
 #include <stdio.h>
 #include <string.h>
-
 /**
  * @brief  signup details before placing order
  * inludes user name, age, password, email, 
  */
-
 struct details {
 	char uname[100];
 	int age;
@@ -23,14 +19,11 @@ struct details {
 	char email[100];
 	char mobile[10];
 };
-
 /**
  * @brief selecting items according to hotels
  * @param first_food shall select the 1st food item 
  * and its quantity
- * 
  */
-
 struct hotels {
 	char hotel[100];
 	char first_food[20];
